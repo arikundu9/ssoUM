91cec6b536bce213521336d8982546dc06500911
Arijit Kundu <arikundu9@gmail.com>
2024-02-15 14:31:07 +0530

    dotnet ef migrations add AppNameAdded

---
+++ Migrations/20240215090051_AppNameAdded.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ssoUM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppNameAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "app_name",
+                table: "app",
+                type: "character(30)",
+                fixedLength: true,
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "app_name",
+                table: "app");
+        }
+    }
+}
