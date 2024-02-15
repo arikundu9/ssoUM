496be33ff51264fcc60d3e97b003d97ecc12ce1f
Arijit Kundu <arikundu9@gmail.com>
2024-02-15 11:21:43 +0530

    v2.10

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -10,7 +10,7 @@ namespace ssoUM.Controllers
 {
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
-    [ApiVersion("2.0")]
+    [ApiVersion("2.10")]
     public class AuthController : ControllerBase
     {
         public IConfiguration Configuration { get; }
--- Controllers/KeyRingController.cs
+++ Controllers/KeyRingController.cs
@@ -1,21 +1,28 @@
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using ssoUM.BAL.Interface;
 
 namespace ssoUM.Controllers;
 
+[Route("api/v{version:apiVersion}/[controller]")]
 [ApiController]
-[Route("api/v1/[controller]")]
+[ApiVersion("2.10")]
 public class KeyRingController : ControllerBase
 {
     private readonly ILogger<KeyRingController> _logger;
 
-    public KeyRingController(ILogger<KeyRingController> logger,IKeyService keyService)
+    public KeyRingController(ILogger<KeyRingController> logger, IKeyService keyService)
     {
         _logger = logger;
     }
 
     [HttpGet]
     public string Get()
+    {
+        return "Online";
+    }
+    [HttpGet, MapToApiVersion(2.0)]
+    public string Get2()
     {
         return "Online";
     }
